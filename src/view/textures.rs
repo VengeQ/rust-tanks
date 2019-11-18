7bb0f7ad28eaf90b168f65f40fd190bbfd6b0cf1
vengeq <vengeq@gmail.com>
2019-11-19 09:42:42 +1000

    add map! macro in textures module

--- src/view/textures.rs
+++ src/view/textures.rs
@@ -2,7 +2,17 @@ use opengl_graphics::{Texture, TextureSettings};
 use std::collections::HashMap;
 use std::path::PathBuf;
 //use graphics::Text;
-
+macro_rules! map {
+    ($($key:expr => $value:expr),*) =>{
+        {
+            let mut map=std::collections::HashMap::new();
+            $(
+                map.insert($key, $value);
+            )*
+            map
+        }
+    };
+}
 
 pub fn texture_creator(texture_settings: &TextureSettings) -> HashMap<String, Texture> {
     let assets = find_folder::Search::ParentsThenKids(3, 3)
@@ -13,12 +23,8 @@ pub fn texture_creator(texture_settings: &TextureSettings) -> HashMap<String, Te
     let ground_texture = create_texture_from_path(texture_settings, "ground.png", &assets);
     let tank_texture = create_texture_from_path(texture_settings, "tank.png", &assets);
 
-    let mut textures = HashMap::new();
-    textures.insert("water".to_owned(), water_texture);
-    textures.insert("wall".to_owned(), wall_texture);
-    textures.insert("ground".to_owned(), ground_texture);
-    textures.insert("tank".to_owned(), tank_texture);
-    textures
+    map!["water".to_owned() => water_texture,"wall".to_owned() => wall_texture,
+         "ground".to_owned() => ground_texture,"tank".to_owned() => tank_texture]
 }
 
 fn create_texture_from_path(texture_settings: &TextureSettings, path: &str, assets: &PathBuf) -> Texture {
@@ -30,7 +36,6 @@ fn create_texture_from_path(texture_settings: &TextureSettings, path: &str, asse
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
     use graphics::ImageSize;
     use piston::window::WindowSettings;
@@ -39,9 +44,10 @@ mod tests {
     use piston_window::PistonWindow;
 
 
-    const OPENGL:OpenGL =OpenGL::V3_2;
-    fn init_gl_helper(gl:OpenGL)->WindowSettings{
-        let  settings:WindowSettings =  WindowSettings::new("Tanks", [640, 640])
+    const OPENGL: OpenGL = OpenGL::V3_2;
+
+    fn init_gl_helper(gl: OpenGL) -> WindowSettings {
+        let settings: WindowSettings = WindowSettings::new("Tanks", [640, 640])
             .graphics_api(gl)
             .resizable(false)
             .exit_on_esc(true);
@@ -50,7 +56,6 @@ mod tests {
 
     #[test]
     fn create_texture_from_path_test() {
-
         let settings = init_gl_helper(OPENGL);
         let _window: PistonWindow<Window> = settings.build().expect("Could not create window");
         let _gl = GlGraphics::new(OPENGL);
@@ -64,7 +69,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected="can't open water_wrong.png")]
+    #[should_panic(expected = "can't open water_wrong.png")]
     fn create_texture_from_wrong_path_test() {
         let settings = init_gl_helper(OPENGL);
         let _window: PistonWindow<Window> = settings.build().expect("Could not create window");
@@ -75,5 +80,15 @@ mod tests {
         let texture_settings = TextureSettings::new().filter(Filter::Nearest);
         let path = "water_wrong.png";
         let _texture = create_texture_from_path(&texture_settings, path, &assets);
+    }
+
+    #[test]
+    fn map_macro_test() {
+        let x: HashMap<String, String> = map![];
+        assert_eq!(x, HashMap::<String, String>::new());
+        let y1 = map![1 => "one".to_owned()];
+        let mut y2 = HashMap::new();
+        y2.insert(1, "one".to_owned());
+        assert_eq!(y1, y2);
     }
 }
